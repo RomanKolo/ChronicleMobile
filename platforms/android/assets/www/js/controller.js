cb49d3321e770ef42990d6331a6a0ee8f0d7fbe9
Roman Kolodziejczyk <kolodz1@gmail.com>
2014-01-12 22:47:26 -0500

    Working filesystem, broken timing

--- platforms/android/assets/www/js/controller.js
+++ platforms/android/assets/www/js/controller.js
@@ -1,5 +1,46 @@
-var chronicleApp = angular.module('chronicleApp', ['ngRoute', 'chronicleControllers', 'ui.bootstrap']);
+var chronicleApp = angular.module('chronicleApp', ['ngRoute', 'ui.bootstrap',]);
 
+chronicleApp.factory('characterDataService', [ '$window', '$route', function(win,rte){
+	var chronicleDir = "Documents/Chronicle2";
+	var dataVar = "SAUSAGES!";
+	
+	function fail(e){
+		console.log(e.code);
+	};
+	
+	return{ 
+		getFile: function(targetFile) {
+			document.addEventListener("deviceready", function() {
+				console.log("ONDEVICEREADY");
+				window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,function(filesystem) {
+				console.log("GOTFS");
+				filesystem.root.getDirectory(chronicleDir, {create: true, exclusive: false}, function(chronicleDirectory){
+					chronicleDirectory.getFile(targetFile, {create: true},  function gotFileEntry(fileEntry) {
+				    	 console.log(fileEntry.fullPath);
+				         fileEntry.file(function readAsText(file) {
+				      	   console.log(file.fullPath);
+				           var reader = new FileReader();
+				           reader.onloadend = function(evt) {
+				               console.log("FILEREAD");
+				               console.log(evt.target.result);
+				               dataVar = evt.target.result;
+				               rte.reload();
+				           };	           
+				           reader.readAsText(file);
+				       }, fail)
+				       }, fail)
+				}, fail)
+				}, fail)
+			}, false);
+			return dataVar;
+		},
+			
+		testService: function(){
+			return chronicleDir;
+		}
+		
+	};
+}]);
 
 
 chronicleApp.config(['$routeProvider', function($routeProvider) {
@@ -9,9 +50,10 @@ chronicleApp.config(['$routeProvider', function($routeProvider) {
 	});
 }]);
 
+/*
 var chronicleControllers = angular.module('chronicleControllers', []);
 var variable = "NADA";
-
+*/
 angular.module('myFilters', [])
 .filter('keys', function() {
   return function(input) {
@@ -22,50 +64,14 @@ angular.module('myFilters', [])
   }
 });
 
-chronicleControllers.controller('chroniclePlayer', ['$scope', '$http', function($scope, $http) {
+chronicleApp.controller('chroniclePlayer', ['$scope', '$http', 'characterDataService', chroniclePlayer]);
+                                            
+function chroniclePlayer($scope, $http, characterData) {
 	console.log("Always Show this line");
-	var fileDataHolder;
-	
-	document.addEventListener("deviceready", onDeviceReady, false);
-	
-    function onDeviceReady() {
-    	console.log("ONDEVICEREADY");
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-    }
-    
-    function gotFS(fileSystem) {
-    	console.log("GOTFS");
-    	fileSystem.root.getDirectory("Documents/Chronicle2", {create: true, exclusive: false}, gotDirectory, fail);
-       // fileSystem.root.getFile("json/testdata.json", null, gotFileEntry, fail);
-    }
-    
-    function gotDirectory(chronicleDirectory) {
-    	chronicleDirectory.getFile("chronicle.json", {create: true}, gotFileEntry, fail)
-    }
-   
-    function gotFileEntry(fileEntry) {
-    	 console.log("GOTFILEENTRY");
-        fileEntry.file(readAsText, fail);
-    }
+	$scope.fileData = characterData.getFile("chronicle.json");
 
-    function readAsText(file) {
-    	   console.log("READ");
-        var reader = new FileReader();
-        reader.onloadend = function(evt) {
-            console.log("Read as text");
-            $scope.fileData = evt.target.result.chronicleJSON.character;
-        };
-        reader.readAsText(file);
-    }
-   // $scope.fileData = fileDataHolder;
-    function fail(e) {
-    	
-    	console.log(e.code);
-    	for (var key in e) {
-    		console.log(key);
-    	}
-    	console.log("ENDE");
-    }
+	
+	
 	
 	$scope.testText = "Blue";
     const CHECKMARK_IMG = "img/30px-MW-Icon-CheckMark.png";
@@ -139,8 +145,10 @@ chronicleControllers.controller('chroniclePlayer', ['$scope', '$http', function(
 		$scope.testText = variable;
 	};
 	
-}]);
-chronicleControllers.$inject = ['$scope', '$http'];
+};
+//chroniclePlayer.$inject = ['$scope', '$http'];
+
+
 
 
 function slowAlert() {
@@ -148,7 +156,36 @@ function slowAlert() {
 	$scope.statusImg = ERROR_IMG;
 };
 
-/*
+/*
+chronicleApp.factory('characterData', function(sourceFile){
+	
+	
+document.addEventListener("deviceready", function() {
+			console.log("ONDEVICEREADY");
+			window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,function(filesystem) {
+			console.log("GOTFS");
+			fileSystem.root.getDirectory("Documents/Chronicle2", {create: true, exclusive: false}, function(chronicleDirectory){
+				chronicleDirectory.getFile("chronicle.json", {create: true},  function gotFileEntry(fileEntry) {
+			    	 console.log("GOTFILEENTRY");
+			         fileEntry.file(function readAsText(file) {
+			      	   console.log("READ");
+			           var reader = new FileReader();
+			           reader.onloadend = function(evt) {
+			               console.log("Read as text");
+			               return evt.target.result.chronicleJSON.character;
+			           };
+			           reader.readAsText(file);
+			       }, fail);, fail)
+			}, fail);
+			}S, fail)
+		};, false)
+		};
+		return readAsText;
+	};;
+}
+})
+
+
 
 
 function fileReadFUnction(file) {
